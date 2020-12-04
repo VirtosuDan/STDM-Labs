a12d11991739ccc933674725bb6acf9193fccefd
VirtosuDan <45829623+VirtosuDan@users.noreply.github.com>
2020-12-04 15:56:34 +0200

    Delete ChangeSummerWheels.java

--- src/BehavioralDP/ChangeSummerWheels.java
+++
@@ -1,8 +0,0 @@
-package BehavioralDP;
-
-public class ChangeSummerWheels implements IChangingWheelsStrategy{
-    @Override
-    public void changeWheels() {
-        System.out.println("If it is a good and warm weather change your wheels to Summer ones!");
-    }
-}
