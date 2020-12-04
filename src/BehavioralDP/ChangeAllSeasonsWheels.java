b2c4da559f138ecb0534d98132a681dd926eefc2
VirtosuDan <45829623+VirtosuDan@users.noreply.github.com>
2020-12-04 15:56:28 +0200

    Delete ChangeAllSeasonsWheels.java

--- src/BehavioralDP/ChangeAllSeasonsWheels.java
+++
@@ -1,9 +0,0 @@
-package BehavioralDP;
-
-public class ChangeAllSeasonsWheels implements IChangingWheelsStrategy{
-
-    @Override
-    public void changeWheels() {
-        System.out.println("If you live in a warm place , change your Wheels in AllSeasons !");
-    }
-}
