c5d0eebf1c9e393e7dc5b7916f312d4284fb0bc1
VirtosuDan <45829623+VirtosuDan@users.noreply.github.com>
2020-12-04 15:56:41 +0200

    Delete ChangeWinterWheels.java

--- src/BehavioralDP/ChangeWinterWheels.java
+++
@@ -1,8 +0,0 @@
-package BehavioralDP;
-
-public class ChangeWinterWheels implements IChangingWheelsStrategy{
-    @Override
-    public void changeWheels() {
-        System.out.println("If the temperature outside is less than 7 grades or is snowing , change to Winter WHeels!");
-    }
-}
